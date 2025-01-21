c31113c21385317bac1abbc7f5ae4a54414fb013
Korisnik <Korisnik@DESKTOP-GTMO6PB>
2025-01-22 00:09:03 +0100

    Add project files.

---
+++ Shortest Path/Graph.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shortest_Path
+{
+    public class Graph : IEnumerable
+    {
+        internal Vertex[] vertices;
+        PartiallyOrderedTree pot;
+
+        public Graph(int[] nodes)
+        {
+            vertices = new Vertex[nodes.Length];
+            pot = new PartiallyOrderedTree(this);
+            for (int i = 0; i < vertices.Length; i++)
+            {
+                vertices[i] = new Vertex(nodes[i]);
+                vertices[i].Index = i + 1;
+            }
+        }
+
+        public void AddEdge(int source, int destination, double cost, DirectionType direction)
+        {
+            vertices[source].AddEdge(destination, cost);
+            if (direction == DirectionType.Undirected)
+                vertices[destination].AddEdge(source, cost);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var vertex in vertices)
+                yield return vertex;
+        }
+
+        public void Display()
+        {
+            foreach (var o in this)
+                Console.WriteLine(o);
+
+            pot.Display();
+
+            Console.ReadLine();
+        }
+
+        public void FindShortestPath()
+        {
+            Console.Write("Enter the starting node: ");
+            int start;
+
+            // Validacija unosa
+            while (!int.TryParse(Console.ReadLine(), out start) || start < 0 || start >= vertices.Length)
+            {
+                Console.WriteLine("Invalid input. Please enter a valid node index (0 to {0}):", vertices.Length - 1);
+            }
+
+            vertices[start].Distance = 0.0;
+
+            for (int i = 1; i <= pot.last; i++)
+            {
+                pot.BubbleUp(i);
+            }
+
+            while (pot.last > 0)
+            {
+                int currentIndex = pot.queue[1]; 
+                Vertex currentVertex = vertices[currentIndex];
+                pot.Swap(1, pot.last--);        
+                pot.BubbleDown(1);              
+
+                foreach (var edge in currentVertex.Neighbors)
+                {
+                    int destination = edge.Destination;
+                    double newDistance = currentVertex.Distance + edge.Cost;
+
+                    if (newDistance < vertices[destination].Distance)
+                    {
+                        vertices[destination].Distance = newDistance; 
+                        pot.BubbleUp(destination + 1);               
+                    }
+                }
+            }
+
+            Display(); 
+        }
+
+
+    }
+}
---
+++ Shortest Path/PartiallyOrderedTree.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shortest_Path
+{
+    public class PartiallyOrderedTree : IEnumerable
+    {
+        internal int[] queue;
+        Graph graph;
+        public int last;
+
+        public PartiallyOrderedTree(Graph graph)
+        {
+            this.graph = graph;
+            last = graph.vertices.Length;
+            queue = new int[last + 1];
+            queue[0] = -1;
+            for (int i = 0; i < last; i++)
+                queue[i + 1] = i;
+        }
+
+        public void Swap(int a, int b)
+        {
+            int temp = queue[a];
+            queue[a] = queue[b];
+            queue[b] = temp;
+        }
+
+        double GetPriority(int a)
+        {
+            return graph.vertices[queue[a]].Distance;
+        }
+
+        internal void BubbleUp(int index)
+        {
+            while (index > 1 && GetPriority(index) < GetPriority(index / 2))
+            {
+                Swap(index, index / 2);
+                index /= 2;
+            }
+        }
+
+        internal void BubbleDown(int index)
+        {
+            while (index * 2 <= last)
+            {
+                int child = index * 2;
+                if (child < last && GetPriority(child) > GetPriority(child + 1))
+                    child++;
+                if (GetPriority(index) <= GetPriority(child))
+                    break;
+                Swap(index, child);
+                index = child;
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var priority in queue)
+            {
+                yield return priority;
+            }
+        }
+
+        public void Display()
+        {
+            Console.Write("Queued: ");
+            for (int i = 1; i <= last; i++)
+                Console.Write(queue[i] + " ");
+            Console.WriteLine();
+            Console.Write("Settled: ");
+            for (int i = last + 1; i < queue.Length; i++)
+                Console.Write(queue[i] + " ");
+            Console.WriteLine();
+        }
+    }
+}
